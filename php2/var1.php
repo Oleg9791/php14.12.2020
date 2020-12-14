f0b1b8605f0c1caddc311cdef2f1e0454106aba4
Oleg9791 <oleg9791@tut.by>
2020-12-14 17:43:13 +0300

    var

---
+++ php2/var1.php
@@ -0,0 +1,45 @@
+<?php
+$many = 1000;
+$percent = 1.5;
+$months = 12;
+$sum = $many;
+
+for ($i = 1; $i <= $months; $i++) {
+    $sum = $sum + ($sum * $percent / 100) / 12;
+    echo "month=" . $i . " sum=" . $sum . "<br>";
+}
+echo round($sum, 2); // оператор for
+// 
+?>
+
+<?php
+echo "<br\n>";
+$many = 1000;
+$percent = 1.5;
+$months = 12;
+$sum = $many;
+$i = 1;
+while ($i <= $months) {
+
+    $sum = $sum + ($sum * $percent / 100) / 12;
+    $i++;
+    echo "months=" . $i . "Sum=" . $sum . "<br>";
+}
+
+echo round($sum, 2);
+?>
+
+<?php
+
+echo "<br\n>";
+$many = 1000;
+$percent = 1.5;
+$months = 12;
+$sum = $many;
+$i = 1;
+do {
+    $sum = $sum + ($sum * $percent / 100) / 12;
+    $i++;
+    echo "monts=" . $i . "Sum=" . $sum . "<br>";
+} while ($i <= $months);
+echo round($sum, 2);
